ed7dbe954b264da8c2dcb9484304d037aa77adb3
beardsley-james <beards18@gmail.com>
2019-10-14 01:07:44 -0400

    i don't know if this is going to work
    i've been drinking and i don't want to test it

--- js/newGenerators.js
+++ js/newGenerators.js
@@ -1,65 +1,26 @@
-var clientGenerator = function(config = {}){
+var opGenerator = function({level = 1, payrate = 500}) {
 
   /* this function accepts a configuration object, all properties are optional
     config = {
-      type = a string from businessTypes or an array of strings from businessTypes
-      frequency = an integer 1-10
-      opLevel = an integer 1-5
-      difficulty = an integer 1-10
-      svcQual = an integer 1-5
-      callRate = an integer greater than 0, represents cents per operator minutes
-    }
-  */
+      level = integer,
+      payrate = integer, in cents
+    } */
 
-  if (!config.type){
-    this.type = businessTypes[Math.floor(Math.random()*businessTypes.length)]
-  } else if (config.type.constructor === Array) {
-    this.type = config.type[Math.floor(Math.random()*config.type.length)]
-  } else {
-    this.type = config.type
-  }
+  this.name = initialsGenerator();
 
-  this.name = generateBusinessName(type);
+  this.level = level;
 
-  if (!config.frequency){
-    this.frequency =  returnNumberInRange(callFreqRanges[type][0], callFreqRanges[type][1])
-  } else {
-    this.frequency = config.frequency
-  }
+  this.payRate = payRate;
 
-  if (!config.opLevel && !config.difficulty){
-    this.opLevel = 1
-  } else if (config.opLevel){
-    this.opLevel = config.opLevel
-  } else if (config.difficulty){
-    this.opLevel = determineOpLevel(difficulty)
-  }
+  this.experience = experience[level - 1][Math.floor(Math.rand() * experience.length)];
 
-  if (!config.svcQual){
-    this.svcQual = Math.floor(Math.random() * 5) + 1
-  } else {
-    this.svcQual = config.svcQual
-  }
+  this.background = backgrounds[level - 1][Math.floor(Math.rand() * backgrounds.length)];
 
-  if (!config.callRate){
-    if (!config.difficulty){
-      this.callRate = Math.floor(Math.random() * (50)) + (10 * this.opLevel)
-    } else {
-      this.callRate = Math.floor(Math.random() * (50)) + (10 * this.difficulty)
-    }
-  } else {
-    this.callRate = config.callRate
-  }
+  this.focus = this.experience.focus + this.background.focus;
 
-  this.callTime = 0;
+  this.personability = this.experience.personability + this.backgrounds.personability;
 
-  this.id = clientId;
-
-  clientId++
-}
-
-// super easy hvac company
-{
-  type: "hvac",
-  difficulty: 1
+  this.call = [];
+  this.idleTime = 0;
+  this.callsCompleted = 0
 }
